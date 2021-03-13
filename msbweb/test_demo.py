f33238987c76515b334edf77630a05057f86ee73
boyuan-oops <848242097@qq.com>
2021-03-13 11:21:10 +0800

    cssselecter

--- msbweb/test_demo.py
+++ msbweb/test_demo.py
@@ -4,6 +4,7 @@ from time import sleep
 
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 class TestDemo:
@@ -20,7 +21,6 @@ class TestDemo:
     def test_demo(self):
         self.driver.get("https://www.meishubao.com/")
         # WebDriverWait(self.driver, 10).until()
-        '''js处理页面'''
 
     @pytest.mark.skip
     def test_js_demo(self):
@@ -36,6 +36,7 @@ class TestDemo:
             print(self.driver.execute_script(code))
         # self.driver.execute_script("return document.title;return JSON.stringify(performance.timing)")
 
+    @pytest.mark.skip
     def test_js_demo1(self):
         '''
         js处理时间，remove readonly属性 重新赋值时间
@@ -46,3 +47,12 @@ class TestDemo:
         self.driver.execute_script("document.getElementById('train_date').value='2021-12-1'")
         sleep(3)
         print(self.driver.execute_script("return document.getElementById('train_date').value"))
+
+    def test_css_demo(self):
+        self.driver.get("https://www.baidu.com/")
+        self.driver.find_element(By.ID, 'kw').send_keys("css")
+        sleep(3)
+        self.driver.find_element(By.ID, 'su').click()
+        sleep(3)
+        self.driver.find_element(By.CSS_SELECTOR, '#s_tab a:nth-child(2)').click()
+        sleep(5)
